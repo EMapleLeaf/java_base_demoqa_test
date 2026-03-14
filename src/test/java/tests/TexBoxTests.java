f95bb78a0c919949bb87ec8f338949fdfa42f8ff
EMapleLeaf <int.mailbox@gmail.com>
2026-03-14 19:46:23 +0300

    Added TestBase

--- src/test/java/tests/TexBoxTests.java
+++ src/test/java/tests/TexBoxTests.java
@@ -1,21 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TexBoxTests {
-    @BeforeAll
-    static void beforeAll(){
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        //Configuration.browserVersion = "144.0";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.timeout = 10000; //default 4000
-    }
+public class TexBoxTests extends TestBase {
 
     @Test
     void successfulFillFormTest() {
