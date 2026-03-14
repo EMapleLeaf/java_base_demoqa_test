0d9f6b63ad79ca226c07ed92e4f392e994724592
EMapleLeaf <int.mailbox@gmail.com>
2026-03-14 19:12:23 +0300

    Added selenide TextBox test

--- src/test/java/tests/TexBoxTests.java
+++ src/test/java/tests/TexBoxTests.java
@@ -1,4 +1,36 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class TexBoxTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        //Configuration.browserVersion = "144.0";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 10000; //default 4000
+    }
+
+    @Test
+    void successfulFillFormTest() {
+        open("/text-box");
+        $("[id=userName]").setValue("Maple Leaf");
+        $("[id=userEmail]").setValue("maple@leaf.com");
+        $("[id=currentAddress]").setValue("first address 1");
+        $("[id=permanentAddress]").setValue("second address 2");
+        $("[id=submit]").click();
+
+        $("[id=output] [id=name]").shouldHave(text("Maple Leaf"));
+        $("[id=output] [id=email]").shouldHave(text("maple@leaf.com"));
+        $("[id=output] [id=currentAddress]").shouldHave(text("first address 1"));
+        $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));
+
+
+    }
 }
