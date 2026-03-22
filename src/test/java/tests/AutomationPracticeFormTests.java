3030ea931916a330c1d506f53b3a2fd2287c6b97
EMapleLeaf <int.mailbox@gmail.com>
2026-03-22 22:40:18 +0300

    Moved testdata to TestData and added files to gitignore

--- src/test/java/tests/AutomationPracticeFormTests.java
+++ src/test/java/tests/AutomationPracticeFormTests.java
@@ -5,56 +5,89 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
+import static tests.testdata.TestData.city;
+import static tests.testdata.TestData.currentAddress;
+import static tests.testdata.TestData.dateOfBirth;
+import static tests.testdata.TestData.fileName;
+import static tests.testdata.TestData.firstName;
+import static tests.testdata.TestData.hobbyMusicName;
+import static tests.testdata.TestData.hobbyReadingName;
+import static tests.testdata.TestData.hobbySportName;
+import static tests.testdata.TestData.lastName;
+import static tests.testdata.TestData.monthOfBirth;
+import static tests.testdata.TestData.sexName;
+import static tests.testdata.TestData.state;
+import static tests.testdata.TestData.subjectName;
+import static tests.testdata.TestData.userEmail;
+import static tests.testdata.TestData.userNumber;
+import static tests.testdata.TestData.yearOfBirth;
 
 public class AutomationPracticeFormTests extends TestBase {
 
     @Test
     void successfulFillPracticeFormTest() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Maple");
-        $("#lastName").setValue("Leaf");
-        $("#userEmail").setValue("maple@leaf.com");
+
+        // удаляем рекламные баннеры
+        executeJavaScript("""
+                document.getElementById('fixedban')?.remove();
+                document.querySelector('footer')?.remove();
+                """);
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
         $("#gender-radio-3").click();
-        $("#userNumber").setValue("9161234567");
+        $("#userNumber").setValue(userNumber);
+
 
         $("#dateOfBirthInput").click();
-        $("#dateOfBirthInput").$("select.react-datepicker__month-select").selectOption(11);
-        $("#dateOfBirthInput").setValue("01 Mar 2014").pressEnter();
+        executeJavaScript("arguments[0].value = '';", $("#dateOfBirthInput"));
+        $("#dateOfBirthInput").sendKeys(dateOfBirth + " " + monthOfBirth + " " + yearOfBirth);
+        $("#dateOfBirthInput").pressEnter();
+//        $("#dateOfBirthInput").$("select.react-datepicker__month-select").selectOption(11);
+//        $("#dateOfBirthInput").setValue("01 Mar 2014").pressEnter();
 
         $("#hobbies-checkbox-1").click();
         $("#hobbies-checkbox-2").click();
         $("#hobbies-checkbox-3").click();
+        $("#uploadPicture").uploadFromClasspath("maple.jfif");
 
-        $("#subjectsContainer #subjectsInput").setValue("111111111");
+        $("#subjectsContainer #subjectsInput").click();
+        $("#subjectsContainer #subjectsInput").setValue("c");
+        $("#subjectsContainer").$(byText(subjectName)).click();
 
-        $("#currentAddress").setValue("Tu-lu-la");
+        $("#currentAddress").setValue(currentAddress);
 
         $("#react-select-3-input").click();
-        $(byText("Haryana")).click();
+        $("#state").$(byText(state)).click();
 
         $("#react-select-4-input").click();
-        $(byText("Panipat")).click();
+        $("#city").$(byText(city)).click();
 
         $("#submit").click();
 
 
-        $("div.modal-body").shouldHave(text("Maple"));
-        $("div.modal-body").shouldHave(text("Leaf"));
-        $("div.modal-body").shouldHave(text("maple@leaf.com"));
-        $("div.modal-body").shouldHave(text("Other"));
-        $("div.modal-body").shouldHave(text("9161234567"));
+        $("div.modal-body").shouldHave(text(firstName));
+        $("div.modal-body").shouldHave(text(lastName));
+        $("div.modal-body").shouldHave(text(userEmail));
+        $("div.modal-body").shouldHave(text(sexName));
+        $("div.modal-body").shouldHave(text(userNumber));
 
-        $("div.modal-body").shouldHave(text("01 Mar 2014"));
+        $("div.modal-body").shouldHave(text(dateOfBirth));
+        $("div.modal-body").shouldHave(text(yearOfBirth));
 
-        $("div.modal-body").shouldHave(text("Sports"));
-        $("div.modal-body").shouldHave(text("Reading"));
-        $("div.modal-body").shouldHave(text("Music"));
+        $("div.modal-body").shouldHave(text(hobbySportName));
+        $("div.modal-body").shouldHave(text(hobbyReadingName));
+        $("div.modal-body").shouldHave(text(hobbyMusicName));
+        $("div.modal-body").shouldHave(text(fileName));
 
-        $("div.modal-body").shouldHave(text("111111111"));
-        $("div.modal-body").shouldHave(text("Tu-lu-la"));
-        $("div.modal-body").shouldHave(text("Haryana"));
-        $("div.modal-body").shouldHave(text("Panipat"));
+
+        $("div.modal-body").shouldHave(text(subjectName));
+        $("div.modal-body").shouldHave(text(currentAddress));
+        $("div.modal-body").shouldHave(text(state));
+        $("div.modal-body").shouldHave(text(city));
 
 
     }
---
+++ src/test/java/tests/testdata/TestData.java
@@ -0,0 +1,20 @@
+package tests.testdata;
+
+public class TestData {
+    public static String firstName = "Maple";
+    public static String lastName = "Leaf";
+    public static String userEmail = "maple@leaf.com";
+    public static String sexName = "Other";
+    public static String userNumber = "9161234567";
+    public static String dateOfBirth = "14";
+    public static String monthOfBirth = "Feb";
+    public static String yearOfBirth = "1988";
+    public static String hobbySportName = "Sports";
+    public static String hobbyReadingName = "Reading";
+    public static String hobbyMusicName = "Music";
+    public static String fileName = "maple.jfif";
+    public static String subjectName = "Computer Science";
+    public static String currentAddress = "We all live in a yellow submarine";
+    public static String state = "Haryana";
+    public static String city = "Panipat";
+}
